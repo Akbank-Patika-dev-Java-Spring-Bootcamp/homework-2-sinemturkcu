b9a2ef111474a4064e4ee60043f610507bb1fd31
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-05-23 23:27:46 +0300

    Add ClientMapper

--- OnlineShoppingSite/src/main/java/com/sinemturkcu/onlineshoppingsite/mapper/ClientMapper.java
+++ OnlineShoppingSite/src/main/java/com/sinemturkcu/onlineshoppingsite/mapper/ClientMapper.java
@@ -1,17 +1,20 @@
 package com.sinemturkcu.onlineshoppingsite.mapper;
 
-import com.sinemturkcu.onlineshoppingsite.dto.ClientDto;
+import com.sinemturkcu.onlineshoppingsite.dto.response.ClientDto;
 import com.sinemturkcu.onlineshoppingsite.dto.request.ClientSaveRequest;
 import com.sinemturkcu.onlineshoppingsite.entity.Client;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ClientMapper {
     ClientMapper INSTANCE = Mappers.getMapper(ClientMapper.class);
 
     Client convertToClient(ClientSaveRequest clientSaveRequest);
-    ClientDto convertToClientDto(Client client);
+    ClientDto convertToClientDTO(Client client);
+    List<ClientDto> convertToClientDTOList(List<Client> clientList);
 
 }
