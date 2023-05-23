acea889a2a9726f86efb17394f9968be67d40450
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-05-23 23:28:00 +0300

    Add ClientService

--- OnlineShoppingSite/src/main/java/com/sinemturkcu/onlineshoppingsite/service/ClientService.java
+++ OnlineShoppingSite/src/main/java/com/sinemturkcu/onlineshoppingsite/service/ClientService.java
@@ -2,13 +2,10 @@ package com.sinemturkcu.onlineshoppingsite.service;
 
 import com.sinemturkcu.onlineshoppingsite.General.BaseEntityService;
 import com.sinemturkcu.onlineshoppingsite.dao.ClientRepository;
-import com.sinemturkcu.onlineshoppingsite.dto.ClientDto;
+import com.sinemturkcu.onlineshoppingsite.dto.response.ClientDto;
 import com.sinemturkcu.onlineshoppingsite.entity.Client;
 import com.sinemturkcu.onlineshoppingsite.mapper.ClientMapper;
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.lang.ref.Cleaner;
 
 
 @Service
@@ -24,6 +21,6 @@ public class ClientService extends BaseEntityService<Client,ClientRepository> {
 
     public ClientDto getByFullName(String name){
         Client client = clientRepository.findByFullName(name);
-        return clientMapper.convertToClientDto(client);
+        return clientMapper.convertToClientDTO(client);
     }
 }
