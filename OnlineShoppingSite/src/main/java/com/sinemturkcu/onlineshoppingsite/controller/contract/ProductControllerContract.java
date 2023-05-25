47ebeed808020dd1f867a01954c85d89ec3bf4c0
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-05-25 22:47:07 +0300

    Add save,findAll and findById methods

---
+++ OnlineShoppingSite/src/main/java/com/sinemturkcu/onlineshoppingsite/controller/contract/ProductControllerContract.java
@@ -0,0 +1,16 @@
+package com.sinemturkcu.onlineshoppingsite.controller.contract;
+
+import com.sinemturkcu.onlineshoppingsite.dto.request.ProductSaveRequest;
+import com.sinemturkcu.onlineshoppingsite.dto.response.ProductDto;
+
+import java.util.List;
+
+public interface ProductControllerContract {
+    ProductDto save(ProductSaveRequest productSaveRequest);
+    List<ProductDto> findAll();
+    ProductDto findById(Long id);
+
+
+
+
+}
