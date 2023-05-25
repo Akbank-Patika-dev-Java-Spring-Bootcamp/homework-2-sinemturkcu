81f9941b8f5bbb18302c3b30016ca2aec12584fa
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-05-25 22:47:51 +0300

    Add @Service annotation

--- OnlineShoppingSite/src/main/java/com/sinemturkcu/onlineshoppingsite/service/ProductService.java
+++ OnlineShoppingSite/src/main/java/com/sinemturkcu/onlineshoppingsite/service/ProductService.java
@@ -3,7 +3,9 @@ package com.sinemturkcu.onlineshoppingsite.service;
 import com.sinemturkcu.onlineshoppingsite.General.BaseEntityService;
 import com.sinemturkcu.onlineshoppingsite.dao.ProductRepository;
 import com.sinemturkcu.onlineshoppingsite.entity.Product;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProductService extends BaseEntityService<Product,ProductRepository> {
     public ProductService(ProductRepository repository) {
         super(repository);
