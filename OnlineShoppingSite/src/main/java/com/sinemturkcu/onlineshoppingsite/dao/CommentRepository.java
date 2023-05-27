88eaea33e0a427ff90929ef0c1f85ba9afc72707
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-05-27 14:17:30 +0300

    Add getCommentsByClientId,getCommentsByProductId

--- OnlineShoppingSite/src/main/java/com/sinemturkcu/onlineshoppingsite/dao/CommentRepository.java
+++ OnlineShoppingSite/src/main/java/com/sinemturkcu/onlineshoppingsite/dao/CommentRepository.java
@@ -3,5 +3,9 @@ package com.sinemturkcu.onlineshoppingsite.dao;
 import com.sinemturkcu.onlineshoppingsite.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment,Long> {
+    List<Comment>  getCommentsByClientId(Long id);
+    List<Comment> getCommentsByProductId(Long id);
 }
