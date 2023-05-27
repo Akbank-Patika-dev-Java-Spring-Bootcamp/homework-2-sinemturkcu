6594d00aae2e6740356633896bc2cf9c1578b0b7
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-05-27 17:11:23 +0300

    Change getByFullName

--- OnlineShoppingSite/src/main/java/com/sinemturkcu/onlineshoppingsite/service/ClientService.java
+++ OnlineShoppingSite/src/main/java/com/sinemturkcu/onlineshoppingsite/service/ClientService.java
@@ -5,22 +5,23 @@ import com.sinemturkcu.onlineshoppingsite.dao.ClientRepository;
 import com.sinemturkcu.onlineshoppingsite.dto.response.ClientDto;
 import com.sinemturkcu.onlineshoppingsite.entity.Client;
 import com.sinemturkcu.onlineshoppingsite.mapper.ClientMapper;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class ClientService extends BaseEntityService<Client,ClientRepository> {
 
-    ClientRepository clientRepository;
-    ClientMapper clientMapper;
+    private final ClientRepository clientRepository;
 
     public ClientService(ClientRepository repository) {
         super(repository);
-        this.clientMapper = clientMapper;
+        clientRepository=repository;
     }
 
-    public ClientDto getByFullName(String name){
+    public Client getByFullName(String name){
         Client client = clientRepository.findByFullName(name);
-        return clientMapper.convertToClientDTO(client);
+        return client;
     }
 }
