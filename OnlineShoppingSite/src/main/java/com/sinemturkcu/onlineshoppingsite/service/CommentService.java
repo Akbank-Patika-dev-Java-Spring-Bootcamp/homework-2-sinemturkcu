5f107bf391c1ab9c900bf0d33ea7f45b55d13189
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-05-27 14:17:53 +0300

    Add getCommentsByClientId,getCommentsByProductId

--- OnlineShoppingSite/src/main/java/com/sinemturkcu/onlineshoppingsite/service/CommentService.java
+++ OnlineShoppingSite/src/main/java/com/sinemturkcu/onlineshoppingsite/service/CommentService.java
@@ -5,10 +5,24 @@ import com.sinemturkcu.onlineshoppingsite.dao.CommentRepository;
 import com.sinemturkcu.onlineshoppingsite.entity.Comment;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CommentService extends BaseEntityService<Comment, CommentRepository> {
+
+    CommentRepository commentRepository;
+
     public CommentService(CommentRepository repository) {
         super(repository);
+        this.commentRepository = repository;
+    }
+
+    public List<Comment> getCommentsByClientId(Long id){
+        return commentRepository.getCommentsByClientId(id);
+    }
+
+    public List<Comment> getCommentsByProductId(Long id){
+        return commentRepository.getCommentsByProductId(id);
     }
 
 }
