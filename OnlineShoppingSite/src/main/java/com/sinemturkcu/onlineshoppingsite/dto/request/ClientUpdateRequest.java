c88c3f4cf61c81e7843ba679265ba8d7e0ff6517
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-05-28 19:11:32 +0300

    Add ClientUpdateRequest

--- OnlineShoppingSite/src/main/java/com/sinemturkcu/onlineshoppingsite/dto/request/ClientUpdateRequest.java
+++ OnlineShoppingSite/src/main/java/com/sinemturkcu/onlineshoppingsite/dto/request/ClientUpdateRequest.java
@@ -1,4 +1,4 @@
 package com.sinemturkcu.onlineshoppingsite.dto.request;
 
-public record ClientUpdateRequest(String fullName, String email, String phoneNumber) {
+public record ClientUpdateRequest(String fullName) {
 }
